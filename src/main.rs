a71a504ff55619fdd74ebaa47a3d29736d5864bc
Oliver Hamlet <oliver@ortham.io>
2021-08-14 10:29:47 +0100

    Fix solving more complex sudokus
    When there are multiple possible values, try each possibility until a
    solution is reached or there are no more possibilities to try.

--- src/main.rs
+++ src/main.rs
@@ -139,14 +139,19 @@ fn print_grid(grid: &Grid) -> String {
 }
 
 fn solve(grid: Grid) -> Grid {
-    let mut working_grid = grid;
+    let mut grid_stack = vec![grid];
+
     let mut loop_count = 0;
     loop {
-        if is_solved(&working_grid) {
-            return working_grid;
-        }
+        let working_grid = match grid_stack.pop() {
+            Some(g) => g,
+            None => panic!("Grid stack is empty, solver is stuck!"),
+        };
 
-        let mut new_grid = working_grid.clone();
+        if is_solved(&working_grid) {
+            println!("Solved sudoku in {} loops!", loop_count);
+            return working_grid.clone();
+        }
 
         'outer: for (row_index, row) in working_grid.iter().enumerate() {
             for (column_index, cell) in row.iter().enumerate() {
@@ -156,35 +161,23 @@ fn solve(grid: Grid) -> Grid {
 
                 let possible_values = get_possible_values(&working_grid, row_index, column_index);
 
-                // println!("Possible values for ({}, {}): {:?}", row_index, column_index, possible_values);
+                for possible_value in possible_values {
+                    // For each possible value, create a new grid and add it to
+                    // the stack, so that each grid is independently looped over
+                    let mut new_grid = working_grid.clone();
+                    new_grid[row_index][column_index] = Some(possible_value);
 
-                if possible_values.is_empty() {
-                    panic!(
-                        "The cell at ({}, {}) has no possible values! Current grid:\n{}",
-                        row_index,
-                        column_index,
-                        print_grid(&working_grid)
-                    );
+                    grid_stack.push(new_grid);
                 }
 
-                if possible_values.len() == 1 {
-                    // println!(
-                    //     "Set value for ({}, {}): {:?}",
-                    //     row_index, column_index, possible_values
-                    // );
-                    new_grid[row_index][column_index] = Some(possible_values[0]);
-                    break 'outer;
-                }
+                // Either the solver got stuck and couldn't find any possible
+                // values, in which case the current grid should be abandoned,
+                // or it added new grid(s) to the stack, in which case they
+                // should now be attempted.
+                break 'outer;
             }
         }
 
-        if working_grid == new_grid {
-            panic!("Infinite loop, grid is unchanged after {} loops:\n{}", loop_count, print_grid(&new_grid));
-        }
-
-        // println!("Replacing working grid with:\n{}", print_grid(&new_grid));
-
-        working_grid = new_grid;
         loop_count += 1;
     }
 }
@@ -282,6 +275,111 @@ mod tests {
                 6 5 9 8 1 2 4 3 7\n\
                 3 2 1 6 7 4 5 9 8\n\
                 7 4 8 3 5 9 2 6 1\n"
+            ),
+            (
+                "9 _ _ _ _ 7 _ _ 3\n\
+                _ _ 3 _ _ 1 8 _ _\n\
+                _ _ _ _ 8 _ _ _ 7\n\
+                7 _ _ _ _ _ 5 _ _\n\
+                4 _ 8 _ 5 _ 7 _ 2\n\
+                _ _ 2 _ _ _ _ _ 1\n\
+                5 _ _ _ 9 _ _ _ _\n\
+                _ _ 1 7 _ _ 2 _ _\n\
+                2 _ _ 6 _ _ _ _ 4\n\n",
+
+                "9 8 5 2 6 7 4 1 3\n\
+                6 7 3 9 4 1 8 2 5\n\
+                1 2 4 5 8 3 6 9 7\n\
+                7 1 9 3 2 4 5 8 6\n\
+                4 6 8 1 5 9 7 3 2\n\
+                3 5 2 8 7 6 9 4 1\n\
+                5 3 6 4 9 2 1 7 8\n\
+                8 4 1 7 3 5 2 6 9\n\
+                2 9 7 6 1 8 3 5 4\n"
+            ),
+            (
+                "_ _ _ _ _ _ _ 2 5\n\
+                _ 9 4 _ 2 _ _ _ _\n\
+                5 _ 7 _ _ 3 _ _ 1\n\
+                1 _ _ _ _ 5 7 _ _\n\
+                _ 3 _ _ _ _ _ 4 _\n\
+                _ _ 8 4 _ _ _ _ 9\n\
+                4 _ _ 7 _ _ 8 _ 2\n\
+                _ _ _ _ 5 _ 9 1 _\n\
+                2 6 _ _ _ _ _ _ _\n\n",
+
+                "8 1 6 9 4 7 3 2 5\n\
+                3 9 4 5 2 1 6 8 7\n\
+                5 2 7 6 8 3 4 9 1\n\
+                1 4 2 8 9 5 7 6 3\n\
+                9 3 5 1 7 6 2 4 8\n\
+                6 7 8 4 3 2 1 5 9\n\
+                4 5 1 7 6 9 8 3 2\n\
+                7 8 3 2 5 4 9 1 6\n\
+                2 6 9 3 1 8 5 7 4\n"
+            ),
+            (
+                "4 _ _ _ _ _ _ _ _\n\
+                _ _ _ 2 1 8 _ 7 _\n\
+                7 _ _ _ 9 _ _ _ 2\n\
+                _ _ 6 _ 3 _ 8 _ 4\n\
+                1 _ _ _ _ _ _ 2 _\n\
+                _ _ 5 _ _ 7 _ _ _\n\
+                _ 1 _ _ 6 _ _ _ _\n\
+                _ 6 _ _ 8 5 _ _ _\n\
+                _ _ 9 _ _ _ _ _ 1\n\n",
+
+                "4 8 2 7 5 3 9 1 6\n\
+                6 9 3 2 1 8 4 7 5\n\
+                7 5 1 4 9 6 3 8 2\n\
+                2 7 6 5 3 1 8 9 4\n\
+                1 3 8 6 4 9 5 2 7\n\
+                9 4 5 8 2 7 1 6 3\n\
+                5 1 4 9 6 2 7 3 8\n\
+                3 6 7 1 8 5 2 4 9\n\
+                8 2 9 3 7 4 6 5 1\n"
+            ),
+            (
+                "2 _ _ _ _ 8 _ 4 5\n\
+                _ 8 _ 6 _ 7 _ _ _\n\
+                9 _ 1 _ _ _ 3 _ _\n\
+                7 _ _ 3 _ _ _ _ _\n\
+                _ _ 8 _ _ 4 1 7 6\n\
+                _ _ 4 1 _ _ 8 _ _\n\
+                5 2 _ _ 9 _ _ _ _\n\
+                _ 3 _ _ 2 _ _ 5 _\n\
+                _ 4 _ _ 6 _ 9 _ 3\n\n",
+
+                "2 6 3 9 1 8 7 4 5\n\
+                4 8 5 6 3 7 2 1 9\n\
+                9 7 1 5 4 2 3 6 8\n\
+                7 1 2 3 8 6 5 9 4\n\
+                3 9 8 2 5 4 1 7 6\n\
+                6 5 4 1 7 9 8 3 2\n\
+                5 2 6 7 9 3 4 8 1\n\
+                8 3 9 4 2 1 6 5 7\n\
+                1 4 7 8 6 5 9 2 3\n"
+            ),
+            (
+                "_ _ _ _ 9 6 _ _ _\n\
+                _ _ _ 3 _ 2 _ 8 5\n\
+                2 _ 4 _ _ 7 _ _ _\n\
+                6 _ _ _ 7 _ _ _ _\n\
+                9 4 _ _ _ 3 7 _ _\n\
+                _ _ _ _ _ _ _ 2 _\n\
+                _ 6 9 _ _ _ _ 4 8\n\
+                _ _ _ 5 3 _ _ _ 7\n\
+                _ 2 _ _ _ _ _ 5 _\n\n",
+
+                "8 5 3 4 9 6 1 7 2\n\
+                7 9 6 3 1 2 4 8 5\n\
+                2 1 4 8 5 7 6 3 9\n\
+                6 3 2 1 7 5 8 9 4\n\
+                9 4 5 2 8 3 7 1 6\n\
+                1 7 8 9 6 4 5 2 3\n\
+                5 6 9 7 2 1 3 4 8\n\
+                4 8 1 5 3 9 2 6 7\n\
+                3 2 7 6 4 8 9 5 1\n"
             )
         ];
 
